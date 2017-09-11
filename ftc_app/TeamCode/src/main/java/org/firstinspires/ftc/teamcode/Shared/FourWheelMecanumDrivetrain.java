70c5df9532f53adefcc1effd1af2be6a4577bf86
Joshua You <youjoshua12@gmail.com>
2017-09-10 22:52:34 -0700

    readded robotcore. oops

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shared/FourWheelMecanumDrivetrain.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.Shared;
+
+/**
+ * Created by Joshua on 9/9/2017.
+ */
+
+public class FourWheelMecanumDrivetrain implements MecanumDrivetrain {
+    RobotHardware rw = RobotHardware.GetSingleton();
+
+    @Override
+    public void MoveCardinal(Direction direction, float speed) {
+
+    }
+
+    @Override
+    public void MoveAngle(float speed, float angle) {
+
+    }
+}

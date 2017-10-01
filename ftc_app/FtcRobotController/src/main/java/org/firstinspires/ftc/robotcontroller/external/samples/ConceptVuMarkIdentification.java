0e829954471ff909b8abfe3b28e6ded5afb80261
buggy213 <gutsylvin@gmail.com>
2017-09-30 18:34:35 -0700

    look at the diff, no idea what this is

--- ftc_app/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptVuMarkIdentification.java
+++ ftc_app/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptVuMarkIdentification.java
@@ -130,7 +130,7 @@ public class ConceptVuMarkIdentification extends LinearOpMode {
         relicTrackables.activate();
 
         while (opModeIsActive()) {
-
+            
             /**
              * See if any of the instances of {@link relicTemplate} are currently visible.
              * {@link RelicRecoveryVuMark} is an enum which can have the following values:
